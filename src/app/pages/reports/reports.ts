c01670dc938d4504bdf9ee7cf012e4fa6797bf1b
Ferdy Fauzi <payen83@gmail.com>
2026-05-08 01:08:54 +0800

    delete report

--- src/app/pages/reports/reports.ts
+++ src/app/pages/reports/reports.ts
@@ -3,6 +3,7 @@ import { SharedModules } from '../../shared/shared.module';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router, RouterLink } from '@angular/router';
 import { Api } from '../../services/api';
+import { Ui } from '../../services/ui';
 
 interface reportItem {
   title: string,
@@ -18,7 +19,7 @@ interface reportItem {
 })
 export class Reports implements OnInit {
   public reportList: reportItem[] = [
-    {title: 'Sample report title 1', category: 'Sample category', date: '01/01/1978'}
+    { title: 'Sample report title 1', category: 'Sample category', date: '01/01/1978' }
   ];
   public dataSource: any = new MatTableDataSource(this.reportList);
   public displayedColumns: string[] = ['no', 'title', 'date', 'actions'];
@@ -26,18 +27,39 @@ export class Reports implements OnInit {
   constructor(
     private router: Router,
     private apiServices: Api,
-    private cdr: ChangeDetectorRef
-  ){}
+    private cdr: ChangeDetectorRef,
+    private ui: Ui
+  ) { }
 
-  async ngOnInit(){
-    try{
+  async ngOnInit() {
+    try {
       let response: any = await this.apiServices.httpGet('/reports');
       console.log(response);
       this.reportList = response.data;
       this.dataSource = new MatTableDataSource(this.reportList);
-      this.dataSource.data = this.dataSource.data.map((report: any) => ({...report}));
+      this.dataSource.data = this.dataSource.data.map((report: any) => ({ ...report }));
       this.cdr.detectChanges();
-    } catch(error){
+    } catch (error) {
+
+    }
+  }
+
+  async onDelete(id: any) {
+    let confirmation = confirm('Are you sure?');
+    if (confirmation) {
+      try {
+        let response: any = await this.apiServices.httpDelete('/reports/delete/' + id);
+        if (response.success) {
+          this.ui.openSnackBar('Deleted', 'OK');
+          let index = this.reportList.findIndex((item: any) => { return item.id == id });
+          this.reportList.splice(index, 1);
+          this.dataSource = new MatTableDataSource(this.reportList);
+          this.dataSource.data = this.dataSource.data.map((report: any) => ({ ...report }));
+          this.cdr.detectChanges();
+        }
+      } catch (error) {
+        console.error(error)
+      }
 
     }
   }
--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -7,59 +7,80 @@ import { Injectable } from '@angular/core';
 export class Api {
   // private baseURL: string = 'https://myexpressapr2026new-production.up.railway.app/api';
   private baseURL: string = 'http://localhost:3000/api';
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
 
   }
 
-  httpGet(path: string){
-    let headers = {headers: new HttpHeaders};
+  httpGet(path: string) {
+    let headers = { headers: new HttpHeaders };
     let fullURL: string = this.baseURL + path;
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
       this.http.get(fullURL, headers).subscribe({
-        next: (response: any)=>{resolve(response)},
-        error: (error: any) => {reject(error)}
+        next: (response: any) => { resolve(response) },
+        error: (error: any) => { reject(error) }
       });
     });
   }
 
-  httpPost(path: string, payload: any, method?: string){
-    let fullURL: string = this.baseURL+path;
-    let headers = {headers: new HttpHeaders};
-    let token: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiZW1haWwiOiJyYWhtYW5AbWFpbC5jb20iLCJpYXQiOjE3NzgxNjg4MDgsImV4cCI6MTc3ODE3MjQwOH0.2wVeTU_A5PBrFQjBgphBxzSxt4N7BE9aNi2jgfZpqtI';
+  httpDelete(path: string) {
+    let token: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiZW1haWwiOiJyYWhtYW5AbWFpbC5jb20iLCJpYXQiOjE3NzgxNzMwMDQsImV4cCI6MTc3ODE3NjYwNH0.nYudzapK2NLRwp2hySA_PXWF-HtIKrMvH8KthHQ9Hb0';
+
+    let fullURL: string = this.baseURL + path;
+    let headers = {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      })};
+    return new Promise((resolve, reject) => {
+      this.http.delete(fullURL, headers).subscribe({
+        next: (response: any) => { resolve(response) },
+        error: (error: any) => { reject(error) }
+      });
+
+    })
+  }
+
+  httpPost(path: string, payload: any, method?: string) {
+    let fullURL: string = this.baseURL + path;
+    let headers = { headers: new HttpHeaders };
+    let token: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiZW1haWwiOiJyYWhtYW5AbWFpbC5jb20iLCJpYXQiOjE3NzgxNzMwMDQsImV4cCI6MTc3ODE3NjYwNH0.nYudzapK2NLRwp2hySA_PXWF-HtIKrMvH8KthHQ9Hb0';
     // Check if payload is FormData (for file uploads)
     const isFormData = payload instanceof FormData;
 
     if (isFormData) {
       payload.append('user_id', '1');
     } else {
-      payload = {...payload, user_id: 1};
+      payload = { ...payload, user_id: 1 };
     }
 
-    if(token){
+    if (token) {
       if (isFormData) {
-        
+
         // For FormData, don't set Content-Type as it will be set automatically with boundary
-        headers = {headers: new HttpHeaders({
-          Authorization: `Bearer ${token}`
-        })};
+        headers = {
+          headers: new HttpHeaders({
+            Authorization: `Bearer ${token}`
+          })
+        };
       } else {
         // For JSON data, set Content-Type to application/json
-        headers = {headers: new HttpHeaders({
-          Authorization: `Bearer ${token}`
-        }).set('Content-Type', 'application/json')};
+        headers = {
+          headers: new HttpHeaders({
+            Authorization: `Bearer ${token}`
+          }).set('Content-Type', 'application/json')
+        };
       }
     }
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
       console.log(payload);
-      if(method == 'put'){
+      if (method == 'put') {
         this.http.put(fullURL, payload, headers).subscribe({
-          next: (response: any)=>{resolve(response)},
-          error: (error: any) => {reject(error)}
+          next: (response: any) => { resolve(response) },
+          error: (error: any) => { reject(error) }
         });
       } else {
         this.http.post(fullURL, payload, headers).subscribe({
-          next: (response: any)=>{resolve(response)},
-          error: (error: any) => {reject(error)}
+          next: (response: any) => { resolve(response) },
+          error: (error: any) => { reject(error) }
         });
       }
 
